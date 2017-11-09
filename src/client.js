be1168700e5ac99e00e52f735e7bece461478eaf
simon-landry <simon.landry100@gmail.com>
2017-11-09 12:27:26 +0100

    fix babelrc, add react helmet

--- src/client.js
+++ src/client.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Helmet from 'react-helmet'; // This should be removed later. It's now here just to show how it works.
 import 'styles/app.scss';
 
 render(
-  <div>Hello World</div>,
+  <div>
+    <Helmet
+      title="Audienti"
+      meta={[{ name: 'description', content: 'This is a reat application.' }]}
+    />
+    Hello World
+  </div>,
   document.getElementById('app-container')
 );

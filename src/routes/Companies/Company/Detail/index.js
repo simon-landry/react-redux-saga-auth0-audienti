8085b2749f48bfb84ab4ab5eed9fccaa85dd0502
simon-landry <simon.landry100@gmail.com>
2017-12-27 17:37:23 +0200

    team card is added

--- src/routes/Companies/Company/Detail/index.js
+++ src/routes/Companies/Company/Detail/index.js
@@ -3,33 +3,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ImmutablePropTypes from 'react-immutable-proptypes';
+import { Card, CardHeader, CardBody, CardTitle, Button } from 'reactstrap';
 
 import { injectIntl } from 'components/Intl';
 import BreadcrumbMenu from 'components/BreadcrumbMenu';
 import HeaderTitle from 'components/HeaderTitle';
-import ButtonLink from 'components/ButtonLink';
 import { selectState } from 'redux/selectors';
 
-export const CompanyDetail = ({ formatMessage, match: { params: { companyId } }, company }) => (
+export const CompanyDetail = ({ formatMessage, company }) => (
   <div className="animated fadeIn">
     <BreadcrumbMenu>
       {formatMessage('{count} {count, plural, one {team} other {teams}}', { count: (company.getIn(['teams', 'length']) || 0) })}
-      <ButtonLink className="no-border" handleClick={console.log} icon="fa fa-gear">
-        {formatMessage('Settings')}
-      </ButtonLink>
     </BreadcrumbMenu>
     <HeaderTitle>{formatMessage('Company {companyName}', { companyName: company.getIn(['attributes', 'name']) || '--' })}</HeaderTitle>
-    {formatMessage('Companies')} {companyId}
+    <Card>
+      <CardHeader>
+        <h3 className="float-left">
+          {formatMessage('Teams')}
+        </h3>
+      </CardHeader>
+      <CardBody className="text-center">
+        <CardTitle>{formatMessage('Teams let you organize work and simplify access to projects.')}</CardTitle>
+        <Button outline color="secondary">
+          {formatMessage('Go to Teams')}
+        </Button>
+      </CardBody>
+    </Card>
   </div>
 );
 
 CompanyDetail.propTypes = {
   formatMessage: PropTypes.func.isRequired,
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      companyId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    }),
-  }).isRequired,
   company: ImmutablePropTypes.mapContains({
     id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   }).isRequired,
